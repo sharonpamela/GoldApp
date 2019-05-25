c34eacc992e4442ab28a9f577cfeacb9f3228ce0
sharonpamela <sharon.pamela.santana@gmail.com>
2019-05-24 23:25:49 -0700

    pushing build

---
+++ client/build/precache-manifest.bce0a9c45c9794477a005f0f6786c075.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebfc8946c696fab01e514fe7e8f8f0be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "202d28e840c2408a4a06",
+    "url": "/static/css/main.a26d43c4.chunk.css"
+  },
+  {
+    "revision": "81aa394c20af28f9f253",
+    "url": "/static/js/2.40f60661.chunk.js"
+  },
+  {
+    "revision": "202d28e840c2408a4a06",
+    "url": "/static/js/main.ac9d61f4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4d012f6ade2ce15464f9cd1997ebff65",
+    "url": "/static/media/gold-melt-2.4d012f6a.jpg"
+  }
+]);
